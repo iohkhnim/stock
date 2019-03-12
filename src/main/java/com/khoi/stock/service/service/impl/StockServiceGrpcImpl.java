5ee9563fcb8e2cc2b5e4700ca9d3292821bd14b2
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-12 11:37:54 +0700

    add grpc server

---
+++ src/main/java/com/khoi/stock/service/service/impl/StockServiceGrpcImpl.java
@@ -0,0 +1,23 @@
+package com.khoi.stock.service.service.impl;
+
+import com.khoi.stock.dao.IStockDAO;
+import com.khoi.stockproto.GetStockRequest;
+import com.khoi.stockproto.GetStockResponse;
+import com.khoi.stockproto.StockServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@GRpcService
+public class StockServiceGrpcImpl extends StockServiceGrpc.StockServiceImplBase {
+
+  @Autowired
+  private IStockDAO stockDAO;
+
+  @Override
+  public void getStock(GetStockRequest request, StreamObserver<GetStockResponse> responseObserver) {
+    responseObserver.onNext(
+        GetStockResponse.newBuilder().setStock(stockDAO.getStock(request.getProductId())).build());
+    responseObserver.onCompleted();
+  }
+}
